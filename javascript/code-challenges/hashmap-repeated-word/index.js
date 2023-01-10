c50c786a341a1e9b3df1d9fd80689ee988a70514
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2023-01-09 22:42:51 -0700

    pushing whiteboard and readme

---
+++ javascript/code-challenges/hashmap-repeated-word/index.js
@@ -0,0 +1,23 @@
+function repeatedWord(str){
+
+  let uniqueWords= [ ];
+
+  let standardString = str.toLoweCase();
+
+
+
+  let splitString = standardString.split(" ");
+
+
+
+  for(let i =0; i < splitString.length; i++){
+
+    let currentWord = splitString[0];
+
+  }
+
+
+}
+
+repeatedWord('Hello there go there we are going to go there');
+
